434ea652f57b7b15f5db2392f3b50c0a68a98e6f
Michael Apfelbeck <m.apfelbeck@gmail.com>
2017-06-07 08:16:49 -0700

    Score plugin now displays.

--- source/screenPlugins/ScorePlugin.hx
+++ source/screenPlugins/ScorePlugin.hx
@@ -4,7 +4,9 @@ import blocks.GameBlock;
 import events.EventAndAction;
 import events.EventManager;
 import events.Events;
+import flixel.addons.ui.FlxUIText;
 import flixel.system.FlxAssets.FlxGraphicAsset;
+import jellyPhysics.math.Vector2;
 import render.IColorSource;
 import screens.BaseScreen;
 
@@ -15,9 +17,11 @@ import screens.BaseScreen;
 private class CountAndTime{
     public var count:Int;
     public var time:Float;
-    public function new(count:Int, time:Float){
+    public var pos:Vector2;
+    public function new(count:Int, time:Float, pos:Vector2){
         this.count = count;
         this.time = time;
+        this.pos = pos;
     }
 }
 class ScorePlugin extends ScreenPluginBase 
@@ -26,10 +30,16 @@ class ScorePlugin extends ScreenPluginBase
     private var lookupTable:Map<Int, CountAndTime> = new Map<Int, CountAndTime>();
     private static var popWaitTime:Float = 1.0;
     
+    private var scoreNumber:Int = 0;
+    private var scoreText:FlxUIText;
+    
     public function new(parent:BaseScreen, colorSource:IColorSource, ?X:Float=0, ?Y:Float=0, ?SimpleGraphic:FlxGraphicAsset) 
     {
         super(parent, X, Y, SimpleGraphic);
         colors = colorSource;
+        
+        scoreText = cast parent.getAsset("score_number");
+        updateScore();
     }
     
     override public function update(elapsed:Float){
@@ -39,6 +49,8 @@ class ScorePlugin extends ScreenPluginBase
             if (lookupTable[key].time <= 0){
                 //trace("Popped " + lookupTable[key].count + " block of type " + key);
                 removeList.add(key);
+                scoreNumber += Std.int(Math.pow(lookupTable[key].count, 3));
+                updateScore();
             }
         }
         for (materialToRemove in removeList){
@@ -52,6 +64,10 @@ class ScorePlugin extends ScreenPluginBase
         eventSet.push(new EventAndAction(Events.BLOCK_POP, onBlockPop));
     }
     
+    private function updateScore():Void{
+        scoreText.text = Std.string(scoreNumber);
+    }
+    
     private function onBlockPop(sender:Dynamic, event:String, args:Array<Dynamic>):Void{
         //trace("Block popped");
         
@@ -61,8 +77,12 @@ class ScorePlugin extends ScreenPluginBase
             if (lookupTable.exists(block.Material)){
                 lookupTable[block.Material].count++;
                 lookupTable[block.Material].time = popWaitTime;
+                lookupTable[block.Material].pos.x *= (lookupTable[block.Material].count / (lookupTable[block.Material].count + 1));
+                lookupTable[block.Material].pos.y *= (lookupTable[block.Material].count / (lookupTable[block.Material].count + 1));
+                lookupTable[block.Material].pos.x += block.DerivedPos.x / (lookupTable[block.Material].count + 1);
+                lookupTable[block.Material].pos.y += block.DerivedPos.y / (lookupTable[block.Material].count + 1);
             }else{
-                lookupTable.set(block.Material, new CountAndTime(1, popWaitTime));
+                lookupTable.set(block.Material, new CountAndTime(1, popWaitTime, block.DerivedPos));
             }
         }
     }
